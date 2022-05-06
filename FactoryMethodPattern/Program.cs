cd813c3f5408a9fe984a43698331ff710d58d907
isak yıldırım <isakyildirim@outlook.com>
2022-05-06 16:35:17 +0300

    Repository düzeltildi

--- FactoryMethodPattern/Program.cs
+++ FactoryMethodPattern/Program.cs
@@ -6,7 +6,7 @@ namespace FactoryMethodPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Deneme");
         }
     }
 }
