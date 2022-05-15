1b6382e2ad63d839a53e8190c00fd873e0c64e85
isak yıldırım <isakyildirim@outlook.com>
2022-05-15 17:15:53 +0300

    Strategy pattern işlendi. geliştirilmek istenilen strateji metoda örneklenmeden verilir. sonra istedilen base class çağrulır.

---
+++ StrategyDesignPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2010();
+            customerManager.SavedCredit();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010 : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("2010before");
+        }
+    }
+    class After2010 : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("2010after");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SavedCredit()
+        {
+            Console.WriteLine("Customer manager bussines");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
