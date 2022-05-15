08c980dd6e99efc1f2888aeb2469cf2b4335081c
isak yıldırım <isakyildirim@outlook.com>
2022-05-15 15:25:04 +0300

    Bridge patterni bussines classında çağrılan base class metodlarının bu base classının miras bıraktığı diğer bussiness classlarının istenildiği gibi kullanılmasını sağlar.

---
+++ BridgeDesignPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerUpdated customer = new CustomerUpdated();
+            customer.MassageSenderBase = new EmailSender();
+            customer.Update();
+            Console.ReadLine();
+        }
+    }
+
+    //Bridge patterni abstract classların bussines clasında çağrılması ve ona bir köprü probu atılması ile yapılır.
+    //bunun sayesinde yapılan işlem gelen köprü clasındaki metodlar ile zenginleştirilebilir
+    
+
+    public abstract class MassageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("MassegeSaved");
+        }
+
+        public abstract void Send(MassageBody massageBody);
+    }
+    
+    public class MassageBody
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }    
+    }
+
+    public class SmsSender : MassageSenderBase
+    {
+        public override void Send(MassageBody massageBody)
+        {
+            Console.WriteLine("{0} massege saved with SmsSender ",massageBody.Title);
+        }
+    }
+    public class EmailSender : MassageSenderBase
+    {
+        public override void Send(MassageBody massageBody)
+        {
+            Console.WriteLine("{0} massege saved with EmailSender", massageBody.Title);
+        }
+    }
+
+    public class CustomerUpdated
+    {
+        public MassageSenderBase MassageSenderBase { get; set; }
+        public void Update()
+        {
+            MassageSenderBase.Send(new MassageBody { Title = "This Update" });
+            Console.WriteLine("Customer Updated");
+        }
+    }
+}
