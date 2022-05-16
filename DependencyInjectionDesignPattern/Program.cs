50c26ebbb451907f8eb005c59cc3b1033870462b
iskyldrm <isakyildirim@outlook.com>
2022-05-16 14:12:44 +0300

    SOLID prensiplerinin sonuncusu yani bağımlılıkların sıfırlanması

---
+++ DependencyInjectionDesignPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new NhProductDal());
+            productManager.Save();
+
+
+            Console.ReadLine();  
+        }
+    }
+    public interface IProductDAL
+    {
+        void Save();
+    }
+
+    public class ProductDAL:IProductDAL
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with EF");
+        }
+    }
+
+    public class NhProductDal : IProductDAL
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+    public class ProductManager
+    {
+        private IProductDAL _productDal;
+
+        public ProductManager(IProductDAL productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
